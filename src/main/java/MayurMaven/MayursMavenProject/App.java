03af682863364f014147593f160e37f54d100304
MayurBilgoji <mayurbilgoji22@gmail.com>
2021-06-19 12:02:56 +0530

    first commit

---
+++ src/main/java/MayurMaven/MayursMavenProject/App.java
@@ -0,0 +1,13 @@
+package MayurMaven.MayursMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MayurMaven/MayursMavenProject/SeleniumTest.java
@@ -0,0 +1,21 @@
+package MayurMaven.MayursMavenProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void browswerAutomation()
+	{
+		System.out.println("browswerAutomation");
+
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+
+	}
+
+}
